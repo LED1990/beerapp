2f1a2e46fd5953366de77a5181031e6cb73cc061
artur <arturpietrucha90@gmail.com>
2020-05-30 17:10:41 +0200

    develop - added some simple integration tests

---
+++ src/main/java/com/microservices/beer/model/errors/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.microservices.beer.model.errors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private String msg;
+}
